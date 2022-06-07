edc4dcc19b2c06d2ef9d40b91949055e65915c18
Anish Bista <anishbista9235@gmail.com>
2022-06-07 09:23:17 +0545

    7th june 2nd

--- vehical-details.php
+++ vehical-details.php
@@ -264,13 +264,13 @@ $_SESSION['brndid']=$result->bid;
       <aside class="col-md-3">
 
         <div class="share_vehicle">
-          <p>Share: <a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a> <a href="#"><i class="fa fa-twitter-square" aria-hidden="true"></i></a> <a href="#"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a> <a href="#"><i class="fa fa-google-plus-square" aria-hidden="true"></i></a> </p>
+          <p>Share: <a href="www.google.com"><i class="fa fa-facebook-square" aria-hidden="true"></i></a> <a href="#"><i class="fa fa-twitter-square" aria-hidden="true"></i></a> <a href="#"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a> <a href="#"><i class="fa fa-google-plus-square" aria-hidden="true"></i></a> </p>
         </div>
         <div class="sidebar_widget">
           <div class="widget_heading">
             <h5><i class="fa fa-envelope" aria-hidden="true"></i>Book Now</h5>
           </div>
-          <form method="post">
+            <form method="post">
             <div class="form-group">
               <input type="text" class="form-control" name="fromdate" placeholder="From Date(dd/mm/yyyy)" required>
             </div>
